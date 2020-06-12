fca1190bbdc7dc0c8fa600a3a748ca249fa5c7c6
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-12 10:46:30 +0200

    Mise en place d'une navbar sticky-top.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,21 +1,23 @@
-// // Création de la barre de navigation sticky-top.
+// Création de la barre de navigation sticky-top.
 
-// // Exécution de la fonction au scroll de la page.
-// window.onscroll = function() {
-//     myFunction()
-// };
+// Exécution de la fonction au scroll de la page.
+window.onscroll = function() {
+    myFunction()
+};
 
-// // Récupération de la navbar.
-// let navbar = document.getElementsByClassName("navbar");
+// Récupération de la navbar.
+var navbar = document.getElementById("navbar");
 
-// // Récupération de la position offset de la navbar.
-// let sticky = navbar.offsetTop;
+// Récupération de la position offset de la navbar.
+var sticky = navbar.offsetTop;
 
-// // Ajout de la class sticky à la navbar quand la position scroll est atteinte et retrait de la class quand la position est quittée.
-// function myFunction() {
-//     if (window.pageYOffset >= sticky) {
-//         navbar.classList.add("sticky")
-//     } else {
-//         navbar.classList.remove("sticky");
-//     }
-// }
+// Ajout de la class sticky à la navbar quand la position scroll est atteinte et retrait de la class quand la position est quittée.
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        if( $('.navbar').hasClass('sticky') ){
+            navbar.classList.remove("sticky");
+        }   
+    }
+}
