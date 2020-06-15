1bb88a2b393b35b46515d4126e30f5069f163787
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-15 23:22:36 +0200

    Mise en forme de la partie expérience avec JS & couleurs.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -21,3 +21,40 @@ function myFunction() {
         }   
     }
 }
+
+
+// Création de la timeline de la rubrique expérience.
+
+(function() {
+
+    'use strict';
+  
+    // Définition de variable.
+    var items = document.querySelectorAll(".timeline li");
+  
+    // Vérification si un élément est dans le viewport.
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+    }
+  
+    // Ecoute d'évènements.
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+  
+  })();
