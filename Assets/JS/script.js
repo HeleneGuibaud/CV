fc58f0bb0fd016ca89b7f5a9732be87016620b7d
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-29 23:10:05 +0200

    Ajout de commentaires JS.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,7 +2,7 @@
 
 // Exécution de la fonction au scroll de la page.
 window.onscroll = function () {
-  myFunction()
+    myFunction()
 };
 
 // Récupération de la navbar.
@@ -13,13 +13,13 @@ var sticky = navbar.offsetTop;
 
 // Ajout de la class sticky à la navbar quand la position scroll est atteinte et retrait de la class quand la position est quittée.
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+    if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
-  } else {
+    } else {
     if ($('.navbar').hasClass('sticky')) {
       navbar.classList.remove("sticky");
     }
-  }
+    }
 }
 
 
@@ -38,18 +38,18 @@ function myFunction() {
   function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
     return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
 
   function callbackFunc() {
     for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
+        if (isElementInViewport(items[i])) {
         items[i].classList.add("in-view");
-      }
+        }
     }
   }
 
