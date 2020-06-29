78a439e45e7d54fadbd91fa257cc99766c65940b
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-06-29 23:19:16 +0200

    Ajout de commentaires CSS.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -14,11 +14,11 @@ var sticky = navbar.offsetTop;
 // Ajout de la class sticky à la navbar quand la position scroll est atteinte et retrait de la class quand la position est quittée.
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
+        navbar.classList.add("sticky");
     } else {
-    if ($('.navbar').hasClass('sticky')) {
-      navbar.classList.remove("sticky");
-    }
+        if ($('.navbar').hasClass('sticky')) {
+            navbar.classList.remove("sticky");
+        }
     }
 }
 
@@ -28,35 +28,35 @@ function myFunction() {
 
 (function () {
 
-  'use strict';
+    'use strict';
 
-  // Définition de variable.
-  var items = document.querySelectorAll(".timeline li");
+    // Définition de variable.
+    var items = document.querySelectorAll(".timeline li");
 
-  // Vérification si un élément est dans le viewport.
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+    // Vérification si un élément est dans le viewport.
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-        if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
         }
     }
-  }
 
-  // Ecoute d'évènements.
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
+    // Ecoute d'évènements.
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
 
 })();
 
@@ -65,7 +65,7 @@ function myFunction() {
 // Fonction pour faire réduire la navbar une fois le click effectué (à mettre à la fin du JS).
 
 $(function () {
-  $('#navbarSupportedContent').on('click', '.nav-item', function () {
-    $('#navbarSupportedContent').toggleClass('show');
-  })
+    $('#navbarSupportedContent').on('click', '.nav-item', function () {
+        $('#navbarSupportedContent').toggleClass('show');
+    })
 })
